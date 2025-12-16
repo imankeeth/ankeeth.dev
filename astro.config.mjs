a512f8dfe2769b8e48433b78bc234190c244cb22
Ankeeth Suvarna <ankitsuvarna11@gmail.com>
2025-12-16 22:52:19 +0530

    Initial Astro setup with React, MDX, Partytown, RSS, Sitemap, Biome, and Cloudflare deployment

---
+++ astro.config.mjs
@@ -0,0 +1,28 @@
+// @ts-check
+import cloudflare from "@astrojs/cloudflare";
+import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+	site: "https://ankeeth.dev",
+	output: "server",
+	adapter: cloudflare({
+		platformProxy: {
+			enabled: true,
+		},
+	}),
+	integrations: [
+		react(),
+		mdx(),
+		partytown({
+			config: {
+				forward: ["dataLayer.push"],
+			},
+		}),
+		sitemap(),
+	],
+});
